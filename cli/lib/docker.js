b985469fcf8127dae479ef1386780d6be0a3269a
jayis1 <jayis1@users.noreply.github.com>
2026-01-26 23:29:54 +0100

    Fix status dots by giving Docker access to Mission Control (v2.2.37)

--- cli/lib/docker.js
+++ cli/lib/docker.js
@@ -162,6 +162,7 @@ export function generateDockerCompose(config) {
       - ${getEnvPath()}
     volumes:
       - ${getConfigDir()}/mission-control/data:/app/data
+      - /var/run/docker.sock:/var/run/docker.sock
     environment:
       - PORT=3030
       - VOICE_APP_URL=http://localhost:${config.server.httpPort || 3000}
