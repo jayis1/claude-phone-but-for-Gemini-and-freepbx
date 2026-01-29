e43485bc18abc5e9863e035700b469ef990a95b9
jayis1 <jayis1@users.noreply.github.com>
2026-01-29 04:12:43 +0100

    release: weedsnacker15 (Refined Data Flow Architecture)

--- install.sh
+++ install.sh
@@ -325,7 +325,7 @@ main() {
 
     cd "$INSTALL_DIR"
     git fetch --tags
-    git reset --hard weedsnacker14
+    git reset --hard weedsnacker15
     
     # Restore .env
     if [ "$HAS_BACKUP" = true ]; then
