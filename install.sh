582ea6f09941f1c4518beff69556fd22328ed3a1
jayis1 <jayis1@users.noreply.github.com>
2026-01-29 04:14:01 +0100

    release: weedsnacker16 (Mesh Inter-AI Architecture)

--- install.sh
+++ install.sh
@@ -325,7 +325,7 @@ main() {
 
     cd "$INSTALL_DIR"
     git fetch --tags
-    git reset --hard weedsnacker15
+    git reset --hard weedsnacker16
     
     # Restore .env
     if [ "$HAS_BACKUP" = true ]; then
